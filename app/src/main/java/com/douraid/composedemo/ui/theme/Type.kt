3ae4e2eef08632eb4a75906adc8059662e09601f
douraidjermachi <douraid.jermachi@kinandcarta.com >
2021-10-09 15:06:51 +0100

    complete the design system

--- app/src/main/java/com/douraid/composedemo/ui/theme/Type.kt
+++ app/src/main/java/com/douraid/composedemo/ui/theme/Type.kt
@@ -2,27 +2,45 @@ package com.douraid.composedemo.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.douraid.composedemo.R
+
+private val NunitoBold = FontFamily(Font(R.font.nunitosans_bold))
+private val NunitoSemiBold = FontFamily(Font(R.font.nunitosans_semibold))
+private val NunitoLight = FontFamily(Font(R.font.nunitosans_light))
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+    h1 = TextStyle(
+        fontFamily = NunitoBold,
+        fontSize = 18.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = NunitoBold,
+        fontSize = 14.sp,
+        letterSpacing = 0.15.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = NunitoLight,
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
+    ),
+    body1 = TextStyle(
+        fontFamily = NunitoLight,
         fontSize = 14.sp
     ),
+    body2 = TextStyle(
+        fontFamily = NunitoLight,
+        fontSize = 12.sp
+    ),
+    button = TextStyle(
+        fontFamily = NunitoSemiBold,
+        fontSize = 14.sp,
+        letterSpacing = 1.sp
+    ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = NunitoSemiBold,
         fontSize = 12.sp
     )
-    */
 )
